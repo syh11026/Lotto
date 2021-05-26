5fc060bd2da903bd7e9b7afa0a86131805194daf
syh11026 <thsdbgus11026@naver.com>
2021-05-26 14:46:55 +0900

    ball image update function

--- app/src/main/java/com/example/lotto/ResultActivity.kt
+++ app/src/main/java/com/example/lotto/ResultActivity.kt
@@ -13,16 +13,35 @@ class ResultActivity : AppCompatActivity() {
         val result = intent.getIntArrayExtra("result")//?: return
         // 2) sort the array
         //result.sort()
-        val result_sorted = result?.sorted()
+       // val result_sorted = result?.sorted()
         // val result_sorted = result?.let { result.sortedBy{it}}
-        /* result?.let{ ArrayList<int!>
-        updateLottoBallImages(result.sortedBy{it})
+         result?.let{
+        updateLottoBallImages(result.sortedBy { it })
         }
-        */
+
         // 3) set images
-        val lottoBallImageStartId = R.drawable.ball_01
+     //   val lottoBallImageStartId = R.drawable.ball_01
        // val lottoBallImageId2 = R.drawable.ball_02
        // val lottoBallImageId3 = R.drawable.ball_03
+   /*     val imageView1 = findViewById<ImageView>(R.id.imageView1)
+        val imageView2 = findViewById<ImageView>(R.id.imageView2)
+        val imageView3 = findViewById<ImageView>(R.id.imageView3)
+        val imageView4 = findViewById<ImageView>(R.id.imageView4)
+        val imageView5 = findViewById<ImageView>(R.id.imageView5)
+        val imageView6 = findViewById<ImageView>(R.id.imageView6)
+
+        imageView1.setImageResource(lottoBallImageStartId + result_sorted!![0] -1)
+        imageView2.setImageResource(lottoBallImageStartId + result_sorted[1] -1)
+        imageView3.setImageResource(lottoBallImageStartId + result_sorted[2] -1)
+        imageView4.setImageResource(lottoBallImageStartId + result_sorted[3] -1)
+        imageView5.setImageResource(lottoBallImageStartId + result_sorted[4] -1)
+        imageView6.setImageResource(lottoBallImageStartId + result_sorted[5] -1)*/
+
+    }
+
+    private fun updateLottoBallImages(result_sorted : List<Int>){
+        val lottoBallImageStartId = R.drawable.ball_01
+
         val imageView1 = findViewById<ImageView>(R.id.imageView1)
         val imageView2 = findViewById<ImageView>(R.id.imageView2)
         val imageView3 = findViewById<ImageView>(R.id.imageView3)
@@ -38,5 +57,4 @@ class ResultActivity : AppCompatActivity() {
         imageView6.setImageResource(lottoBallImageStartId + result_sorted[5] -1)
 
     }
-
 }
