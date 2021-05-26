46247dfa68ec0fac5845f9e998c39c8f2f22619f
syh11026 <thsdbgus11026@naver.com>
2021-05-26 15:12:28 +0900

    shuffling

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -32,14 +32,16 @@ fun getRandomLottoNumbers(): MutableList<Int>{
     }
     return lottoNumbers
 }
-fun getShuffledLottoNumbers () : MutableList<Int>{
+fun  getShuffledLottoNumbers () : MutableList<Int>{
+    val list = mutableListOf<Int>()
     for(number in 1..45){
         list.add(number)
     }
     list.shuffle()
-    return list.subList(0,6)
 
+    return list.subList(0,6)
 }
+
 
 
 class MainActivity : AppCompatActivity() {
